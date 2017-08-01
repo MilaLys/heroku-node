0e8c0c38174b64b8c61a5acffff329728ffd386d
Mila <llysenko23@rambler.ru>
2017-08-01 15:16:08 +0300

    +files

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-var express = require('express');
+var express = require( 'express' );
 var app = express();
 
 // set the port of our application
@@ -6,18 +6,18 @@ var app = express();
 var port = process.env.PORT || 8080;
 
 // set the view engine to ejs
-app.set('view engine', 'ejs');
+//app.set( 'view engine', 'ejs' );
 
 // make express look in the public directory for assets (css/js/img)
-app.use(express.static(__dirname + '/public'));
+app.use( express.static( __dirname + '/public' ) );
 
 // set the home page route
-app.get('/', function(req, res) {
+app.get( '/', function ( req, res ) {
+    
+    // ejs render automatically looks in the views folder
+    res.sendFile(path.join(__dirname, '/public', 'index.html'));
+} );
 
-	// ejs render automatically looks in the views folder
-	res.render('index');
-});
-
-app.listen(port, function() {
-	console.log('Our app is running on http://localhost:' + port);
-});
+app.listen( port, function () {
+    console.log( 'Our app is running on http://localhost:' + port );
+} );
